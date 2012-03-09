5440f8e32495b2d048abd503fe7e9a88b23f141b
Anders Brownworth <anders@droneos.com>
2012-03-08 19:28:58 -0500

    add a random static offset so tracks diverge significantly

--- send.js
+++ send.js
@@ -5,13 +5,15 @@ if ( process.argv[2] != null ) {
     var id = process.argv[2];
     var lat = 42.3599120000 + (Math.random() / 100);
     var lng = -71.0875660000 + (Math.random() / 100);
+    var latO = (Math.random() - .5) / 1000;
+    var lngO = (Math.random() -.5) / 1000;
     var data;
 
     console.log('Sending random data for id ' + id);
 
     setInterval( function() {
-	    lat += (Math.random() / 1000);
-	    lng += (Math.random() / 1000);
+	    lat += (Math.random() / 1000) + latO;
+	    lng += (Math.random() / 1000) + lngO;
 
 	    data = { latitude: lat, longitude: lng, altitude: 10 };
 	    client.publish(id, JSON.stringify( data ) );

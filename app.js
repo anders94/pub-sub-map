58f6a68e135b3f0224344766102e69358b8373dd
Anders Brownworth <anders-git@evantide.com>
2012-04-09 05:09:18 -0700

    removed mongodb - going to do history (last 100 points) with redis

--- app.js
+++ app.js
@@ -2,8 +2,7 @@
  * settings
  */
 
-var webPort = 8090;
-var history = true;
+var webPort = 8070;
 
 /**
  * Module dependencies.
@@ -13,10 +12,6 @@ var express = require('express')
   , routes = require('./routes');
 var io = require('socket.io');
 var redis = require('redis');
-var mongo = require('mongodb'),
-    Db         = mongo.Db,
-    Connection = mongo.Connection,
-    Server     = mongo.Server;
 
 var app = module.exports = express.createServer(),
     io = io.listen(app);
@@ -43,106 +38,69 @@ app.configure('production', function(){
 // Routes
 
 app.get('/', routes.index);
+app.post('/points.json', routes.points);
 
 app.listen(webPort);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 
-// mongodb / socket.io
-var db = new Db('points', new Server('127.0.0.1', 27017, {}), {});
-db.open(function(err, db) {
-	if (err) {
-	    console.log('ERROR: ',err);
-	}
-	else {
-	    db.collection('data', function(err, collection) {
-		    if (err) {
-			console.log('ERROR: ', err);
+// socket.io
+io.sockets.on('connection', function (socket) {
+	console.log('Connection: ' + socket.id);
+
+	var rc = redis.createClient();
+	var keys = new Array();
+	var limit = 10;
+
+	socket.on('sub', function(key) {
+		// enforce alphanumeric keys
+		var rx = new RegExp(/\W/);
+		var a = key.match(rx);
+		if (a == null && key != '') {
+		    if (keys.indexOf(key) < 0) {
+			if (keys.push(key) > limit)
+			    rc.unsubscribe('feed.'+keys.pop());
+			rc.subscribe('feed.'+key);
 		    }
-		    else {
-			io.sockets.on('connection', function (socket) {
-				console.log('Connection: ' + socket.id);
+		    console.log('sub: '+key+' ('+keys.length+' subscriptions: '+keys+')');
+		}
+		else
+		    console.log('ERROR: illegal subscription '+key);
 
-				var rc = redis.createClient();
-				var keys = new Array();
-				var limit = 10;
+	    });
 
-				socket.on('sub', function(key) {
-					// enforce alphanumeric keys
-					var rx = new RegExp(/\W/);
-					var a = key.match(rx);
-					if (a == null && key != '') {
-					    if (keys.indexOf(key) < 0) {
-						if (keys.push(key) > limit)
-						    rc.unsubscribe('feed.'+keys.pop());
-						if (history) {
-						    var h = new Array();
-						    collection.find({key:key}).sort({_id:-1}).limit(10, function(err, cursor){
-							    cursor.each(function(err, data) {
-								    if (err)
-									console.log('error: ', err);
-								    else if (data) {
-									// top level of 'data' here is a {} not a []
-									var a = new Array();
-									a.push(data);
-									h.push(a);
-								    }
-								    else {
-									for (var x=0; x<h.length; x++){
-									    var d = h.pop();
-									    console.log(JSON.stringify(d));
-									    socket.volatile.emit('data', d);
-									}
-								    }
-								});
-							});
-						}
-						rc.subscribe('feed.'+key);
-					    }
-					    console.log('sub: '+key+' ('+keys.length+' subscriptions: '+keys+')');
-					}
-					else {
-					    console.log('ERROR: illegal subscription '+key);
-					}
+	socket.on('unsub', function(key) {
+		var i = keys.indexOf(key);
+		if (i > -1) {
+		    var rest = keys.slice(i + 1 || keys.length);
+		    keys.length = i < 0 ? keys.length + i : i;
+		    keys.push.apply(keys, rest);
+		    rc.unsubscribe('feed.'+key);
 
-				    });
+		}
+		console.log('unsub: ' + key + ' (' + keys.length + ' subscriptions: ' + keys + ')');
 
-				socket.on('unsub', function(key) {
-					var i = keys.indexOf(key);
-					if (i > -1) {
-					    var rest = keys.slice(i + 1 || keys.length);
-					    keys.length = i < 0 ? keys.length + i : i;
-					    keys.push.apply(keys, rest);
-					    rc.unsubscribe('feed.'+key);
+	    });
 
-					}
-					console.log('unsub: ' + key + ' (' + keys.length + ' subscriptions: ' + keys + ')');
+	socket.on('subs', function(key) {
+		socket.emit('info', keys);
+		console.log('subs: ' + keys +'');
 
-				    });
+	    });
 
-				socket.on('subs', function(key) {
-					socket.emit('info', keys);
-					console.log('subs: ' + keys +'');
+	socket.on('disconnect', function () {
+		console.log('disconnect');
+		rc.end();
 
-				    });
+	    });
 
-				socket.on('disconnect', function () {
-					console.log('disconnect');
-					rc.end();
+	rc.on('message', function(key, data) {
+		var o = JSON.parse(data);
+		console.log('rc.on(data): ' + JSON.stringify(o));
+		socket.volatile.emit('data', o);
+	    });
 
-				    });
+	rc.on( 'error', function(err) {
+		console.log('Redis Error: ' + err );
 
-				rc.on('message', function(key, data) {
-					var o = JSON.parse(data);
-					console.log('rc.on(data): ' + JSON.stringify(o));
-					socket.volatile.emit('data', o);
-				    });
-
-				rc.on( 'error', function(err) {
-					console.log('Redis Error: ' + err );
-
-				    });
-			    });
-		    }
-		});
-	}
+	    });
     });

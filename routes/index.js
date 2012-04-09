64fb94fbed902d5ab175dbf7f05d045cc6f32f66
Anders Brownworth <anders-git@evantide.com>
2012-04-08 20:55:56 -0700

    route for points post

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,39 @@
+var redis = require('redis');
 
 /*
  * GET home page.
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'pub-sub-map' })
+    res.render('index', { title: 'pub-sub-map' })
 };
+
+
+/*
+ * POST points.json
+ */
+
+
+exports.points = function(req, res){
+    var json = req.body;
+    if(json.key != null && json.key != ''){
+	var key = json.key;
+	if(json.points != null && json.points != ''){
+	    var points = json.points;
+	    for(var x=0; x<points.length; x++){
+		var point = points[x];
+		if(rational(point.latitude) && rational(point.longitude))
+		    console.log(point.longitude + ', ' + point.latitude);
+	    }
+	}
+    }
+	    
+    res.send('{"message":true}');
+}
+
+function rational(x){
+    if(x != null && x != '' && x > -180 && x < 180)
+	return true
+    else
+        return false
+}
